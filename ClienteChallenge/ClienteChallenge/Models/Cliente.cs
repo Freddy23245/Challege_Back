1b26e488bd9618ee8c89756b7c51a9303ed88b9a
Freddy23245 <cyberdyne710@gmail.com>
2024-12-24 07:47:33 -0300

    correccion en los dataAnnotations

--- ClienteChallenge/ClienteChallenge/Models/Cliente.cs
+++ ClienteChallenge/ClienteChallenge/Models/Cliente.cs
@@ -7,10 +7,10 @@ namespace ClienteChallenge.Models
     {
         public int Id { get; set; }
         [Required(ErrorMessage = "El campo Nombres es obligatorio.")]
-        [StringLength(100, ErrorMessage = "El campo Nombres no puede tener más de 50 caracteres.")]
+        [StringLength(50, ErrorMessage = "El campo Nombres no puede tener más de 50 caracteres.")]
         public string Nombres { get; set; } = null;
         [Required(ErrorMessage = "El campo Apellidos es obligatorio.")]
-        [StringLength(100, ErrorMessage = "El campo Apellidos no puede tener más de 100 caracteres.")]
+        [StringLength(80, ErrorMessage = "El campo Apellidos no puede tener más de 80 caracteres.")]
         public string Apellidos { get; set; } = null!;
         [Required]
         [DataType(DataType.Date)]
@@ -18,10 +18,11 @@ namespace ClienteChallenge.Models
         [Required(ErrorMessage = "El campo CUIT es obligatorio.")]
         [RegularExpression(@"^\d{2}-\d{8}-\d{1}$", ErrorMessage = "El CUIT debe tener el formato XX-XXXXXXXX-X.")]
         public string Cuit { get; set; } = null!;
-      
+        [StringLength(50, ErrorMessage = "El campo Domicilio no puede tener más de 50 caracteres.")]
         public string Domicilio { get; set; } = null!;
         [Required(ErrorMessage = "El campo Teléfono Celular es obligatorio.")]
         [Phone(ErrorMessage = "El número de teléfono no tiene el formato válido.")]
+        [StringLength(10, ErrorMessage = "El campo 10 no puede tener más de 10 caracteres.")]
         public string Celular { get; set; } = null!;
         [Required(ErrorMessage = "El campo Email es obligatorio.")]
         [EmailAddress(ErrorMessage = "El Email no tiene el formato correcto.")]
